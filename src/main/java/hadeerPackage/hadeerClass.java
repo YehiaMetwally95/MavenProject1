43b1ed1b64e998d7da970f19fadbec7b268e3c1f
yehia.metwally <jmetwallym@gmail.com>
2026-04-13 03:46:01 +0200

    Test

--- src/main/java/hadeerPackage/hadeerClass.java
+++ src/main/java/hadeerPackage/hadeerClass.java
@@ -52,17 +52,32 @@ public class hadeerClass {
 
 
 
-        // Find the FirSearch Result Locator by xPath attribute and then store it in "By variable"
+        // Find the Result Locator by xPath attribute and then store it in "By variable"
         By searchResultLocator1 = By.xpath("(//h3)[1]");
+        By searchResultLocator2 = By.xpath("(//h3)[2]");
+        By searchResultLocator3 = By.xpath("(//h3)[3]");
+        By searchResultLocator4 = By.xpath("(//h3)[4]");
 
         // Give the Search Result locator to Selenium Driver to Find the Search Result WebElement
         WebElement searchResult1 = driver.findElement(searchResultLocator1);
+        WebElement searchResult2 = driver.findElement(searchResultLocator2);
+        WebElement searchResult3 = driver.findElement(searchResultLocator3);
+        WebElement searchResult4 = driver.findElement(searchResultLocator4);
 
         // Take the Read Text Action on Search Button WebElement
         String searchResult1Text = searchResult1.getText();
+        String searchResult2Text = searchResult2.getText();
+        String searchResult3Text = searchResult3.getText();
+        String searchResult4Text = searchResult4.getText();
         System.out.println(searchResult1Text);
+        System.out.println(searchResult2Text);
+        System.out.println(searchResult3Text);
+        System.out.println(searchResult4Text);
 
         // Compare between Actual and Expected, Where the Actual Text "searchResult1Text" contains the Expected Text "Yehia Metwally"
         Assert.assertTrue(searchResult1Text.contains("Yehia Metwally"));
+        Assert.assertTrue(searchResult2Text.contains("Yehia Metwally"));
+        Assert.assertTrue(searchResult3Text.contains("Yehia Metwally"));
+        Assert.assertTrue(searchResult4Text.contains("Yehia Metwally"));
     }
 }
